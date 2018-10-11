ebc73cf32c4d6fbccc26cc756f8618c4bf58354f
hongjie <32968210@qq.com>
2018-10-11 17:52:40 +0800

    stockx 爬虫搞定

--- stockx.py
+++ stockx.py
@@ -10,6 +10,7 @@ import os
 import time
 from threading import Thread
 from Queue import Queue
+from pyquery import PyQuery
 
 
 error_detail_url = {}
@@ -31,7 +32,6 @@ class PageSpider(Thread):
             products = json_data.get('Products')
             for p in products:
                 self.q.put('https://stockx.com/%s' % p.get('urlKey'))
-                print('https://stockx.com/%s' % p.get('urlKey'))
         except:
             helper.log('[ERROR] => ' + self.url, 'stockx')
             self.error_page_url_queue.put(self.url)
@@ -50,39 +50,63 @@ class GoodsSpider(Thread):
         '''
         解析网站源码
         '''
-        # try:
-        #     pq = helper.get(self.url, myHeaders=self.headers)
-        #     # 款型名称
-        #     name = pq('input.bVProductName').attr('value')
-        #     # 配色的编号
-        #     span = pq('div#styleColors span.styleColorIds')
-        #     number = span.text().strip().replace('- ', '')
-        #     number = re.sub(re.compile(r'\s'), ' ', number)
-        #     number = ''.join(number.split())
-        #     span = pq('div#productPrices span')
-        #     price = span.text().replace('$', '').split(' ')[0]
-        #     try:
-        #         price = float(price)
-        #     except:
-        #         price = 0.0
-        #     aria_label_list = pq('div#productSizes button')
-        #     size_price_arr = [{'size': float(re.compile(r'\d+\.[05]').findall(a.get('aria-label'))[0]), 'price': price, 'isInStock': 'unavailable' not in a.get('aria-label')} for a in aria_label_list]
-        #     mongo.insert_pending_goods(name, number, self.url, size_price_arr, ['%s.jpg' % number], self.gender, '', 'stockx', '5bace180c7e854cab4dbcc83', self.crawl_counter)
-        #     # 下载图片
-        #     img_list = pq('div.pdp-image')
-        #     img_url = 'https:' + (img_list[2].get('data-large') if len(img_list) > 2 else img_list[-1].get('data-large'))
-        #     result = helper.downloadImg(img_url, os.path.join('.', 'imgs', 'stockx', '%s.jpg' % number))
-        #     if result == 1:
-        #         # 上传到七牛
-        #         qiniuUploader.upload_2_qiniu('stockx', '%s.jpg' % number, './imgs/stockx/%s.jpg' % number)
-        # except:
-        #     global error_detail_url
-        #     error_counter = error_detail_url.get(self.url, 1)
-        #     error_detail_url[self.url] = error_counter + 1
-        #     helper.log('[ERROR] error timer = %s, url = %s' % (error_counter, self.url), 'stockx')
-        #     if error_counter <= 3:
-        #         self.q.put(self.url)
-        pass
+        try:
+            pq = helper.get(self.url)
+            # 款型名称
+            name = pq('h1.name').text()
+            number = ''
+            color_value = ''
+            price = 0.0
+            for div in pq('div.detail'):
+                div = PyQuery(div)
+                key = div.find('span.title').text()
+                if key == 'Style':
+                    # 配色的编号
+                    number = div.find('span')[-1].text.strip()
+                elif key == 'Colorway':
+                    color_value = div.find('span')[-1].text.strip()
+                elif key == 'Retail Price':
+                    price = div.find('span')[-1].text.replace('US$', '').strip()
+                    price = float(price)
+            # 找出所有尺寸
+            size_price_arr = []
+            div_list = PyQuery(pq('div.select-options')[0]).find('div.inset div')
+            for i in xrange(0, len(div_list), 2):
+                if div_list[i].text == 'All':
+                    continue
+                size_price_arr.append({
+                    'size': div_list[i].text,
+                    'price': price if div_list[i + 1].text == 'Bid' else float(div_list[i + 1].text.replace('US$', '').replace(',', '').strip()),
+                    'isInStock': True
+                })
+            mongo.insert_pending_goods(name, number, self.url, size_price_arr, ['%s.jpg' % number], 0, color_value, 'stockx', '5bace180c7e854cab4dbcc83', self.crawl_counter)
+            # 下载图片
+            img_url = ''
+            img_list = pq('div.image-container img')
+            if img_list:
+                img_url = img_list[-1].get('src')
+            else:
+                img_url = pq('div.product-media img').attr('src')
+            img_url_list = img_url.split('?')
+            img_url_query_list = img_url_list[1].split('&')
+            for i in xrange(0, len(img_url_query_list)):
+                if img_url_query_list[i].split('=')[0] == 'w':
+                    img_url_query_list[i] = 'w=600'
+                elif  img_url_query_list[i].split('=')[0] == 'h':
+                    img_url_query_list[i] = 'h=600'
+            img_url = img_url_list[0] + '?' + '&'.join(img_url_query_list)
+            print(img_url)
+            result = helper.downloadImg(img_url, os.path.join('.', 'imgs', 'stockx', '%s.jpg' % number))
+            if result == 1:
+                # 上传到七牛
+                qiniuUploader.upload_2_qiniu('stockx', '%s.jpg' % number, './imgs/stockx/%s.jpg' % number)
+        except:
+            global error_detail_url
+            error_counter = error_detail_url.get(self.url, 1)
+            error_detail_url[self.url] = error_counter + 1
+            helper.log('[ERROR] error timer = %s, url = %s' % (error_counter, self.url), 'stockx')
+            if error_counter <= 3:
+                self.q.put(self.url)
 
 
 def fetch_page(url_list, q, error_page_url_queue, crawl_counter):
@@ -125,23 +149,14 @@ def start():
     json_data = json.loads(json_txt)
     pagination = json_data.get('Pagination')
     total_page = pagination.get('lastPage')
-    fetch_page(['https://stockx.com/api/browse?order=DESC&page=%d&productCategory=sneakers&sort=release_date' % page for page in xrange(1, total_page)], q, error_page_url_queue, crawl_counter)
+    fetch_page(['https://stockx.com/api/browse?order=DESC&page=%d&productCategory=sneakers&sort=release_date' % page for page in xrange(1, total_page + 1)], q, error_page_url_queue, crawl_counter)
 
-    # # 处理出错的链接
-    # while not error_page_url_queue.empty():
-    #     error_page_url_list = []
-    #     while not error_page_url_queue.empty():
-    #         error_page_url_list.append(error_page_url_queue.get())
+    # 处理出错的链接
+    while not error_page_url_queue.empty():
+        error_page_url_list = []
+        while not error_page_url_queue.empty():
+            error_page_url_list.append(error_page_url_queue.get())
 
-    #     error_page_men_url_list = [{'url': url_data.get('url'), 'count': url_data.get('count')} for url_data in error_page_url_list if url_data.get('gender') == 1]
-    #     fetch_page([{'url': url_data.get('url'), 'count': url_data.get('count')} for url_data in error_page_men_url_list], 1, q, error_page_url_queue, {
-    #         'mnid': 'men_shoes',
-    #         'Ns': 'sku.bestSeller | 1',
-    #         'isAjax': 'true'
-    #     }, crawl_counter)
-    #     error_page_women_url_list = [{'url': url_data.get('url'), 'count': url_data.get('count')} for url_data in error_page_url_list if url_data.get('gender') == 2]
-    #     fetch_page([{'url': url_data.get('url'), 'count': url_data.get('count')} for url_data in error_page_women_url_list], 2, q, error_page_url_queue, {
-    #         'mnid': 'women_shoes',
-    #         'isAjax': 'true',
-    #     }, crawl_counter)
+        fetch_page(error_page_url_list, q, error_page_url_queue, crawl_counter)
+
     helper.log('done', 'stockx')
